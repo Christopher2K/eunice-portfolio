776616168ed7e821edfc27a1d7d1ef202f1532c6
Christopher N. KATOYI KABA <me@christopher2k.dev>
2025-10-07 21:16:39 -0400

    chore: initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { css } from "styled-system/css";
+
+export const Route = createFileRoute("/")({
+  component: App,
+});
+
+function App() {
+  return <div className={css({ backgroundColor: "green.400" })}>Hello</div>;
+}
