c96a51d0a72d7a3c7188919ccaa62911f4ba1074
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2025-12-13 16:10:26 -0500

    feat: devenv setup

---
+++ devenv.nix
@@ -0,0 +1,55 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  env = {
+    POSTGRES_USER = "postgres";
+    POSTGRES_PASSWORD = "postgres";
+    POSTGRES_DB = "eunice-cms";
+    DATABASE_URI = "postgres://postgres:postgres@localhost:5432/eunice-cms";
+
+    S3_HOST = "http://127.0.0.1:9000";
+    S3_ACCESS_KEY_ID = "bcsjX40Ze53gINavlU2y";
+    S3_ACCESS_SECRET_KEY = "Sa6RNTBGbm9w35DIYfvLFQ4ApHUZe1MVW27CdcEP";
+    S3_BUCKET = "payload-cms";
+    S3_REGION = "us-east-1";
+
+    PAYLOAD_URI = "http://localhost:3001";
+  };
+
+  services.postgres = {
+    enable = true;
+    package = pkgs.postgresql_17_jit;
+    listen_addresses = "127.0.0.1";
+    initialDatabases = [
+      {
+        name = config.env.POSTGRES_DB;
+        pass = config.env.POSTGRES_PASSWORD;
+        user = config.env.POSTGRES_USER;
+      }
+    ];
+    port = 5432;
+  };
+
+  services.minio = {
+    enable = true;
+    region = config.env.S3_REGION;
+    accessKey = config.env.S3_ACCESS_KEY_ID;
+    secretKey = config.env.S3_ACCESS_SECRET_KEY;
+    buckets = [ config.env.S3_BUCKET ];
+  };
+
+  # https://devenv.sh/scripts/
+  processes."dev:cms".exec = ''
+    pnpm --filter cms dev
+  '';
+
+  processes."dev:portfolio".exec = ''
+    pnpm --filter portfolio dev
+  '';
+}
