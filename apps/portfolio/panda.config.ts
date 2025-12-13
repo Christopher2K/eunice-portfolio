fabf12e651952f109d38fa9ee2a858f3cd5d3c6c
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2025-12-13 01:32:10 -0500

    fix: dockerfile

--- apps/portfolio/panda.config.ts
+++ apps/portfolio/panda.config.ts
@@ -1,10 +1,147 @@
 import {
   defineConfig,
-  defineGlobalStyles,
   defineGlobalFontface,
+  defineGlobalStyles,
+  defineRecipe,
 } from "@pandacss/dev";
-import { textRecipe } from "./src/ui/recipes/text";
-import { buttonRecipe } from "./src/ui/recipes/button";
+
+export const textRecipe = defineRecipe({
+  className: "text",
+  description: "Text styles in the design system",
+  base: {},
+  variants: {
+    variant: {
+      heading1: {
+        fontFamily: "lausanne",
+        fontWeight: 300,
+        fontSize: "80px",
+        lineHeight: 1.5,
+        letterSpacing: "-2%",
+      },
+      heading2: {
+        fontFamily: "lausanne",
+        fontWeight: 300,
+        fontSize: "64px",
+        lineHeight: 1.5,
+      },
+      heading3: {
+        fontFamily: "lausanne",
+        fontWeight: 300,
+        fontSize: "48px",
+        lineHeight: 1.5,
+      },
+      heading4: {
+        fontFamily: "lausanne",
+        fontWeight: 300,
+        fontSize: "32px",
+        lineHeight: 1.5,
+      },
+      subhead: {
+        fontFamily: "space",
+        fontWeight: 400,
+        fontSize: "14px",
+        lineHeight: 1.5,
+        letterSpacing: "0.04em",
+        textTransform: "uppercase",
+      },
+      smallSubhead: {
+        fontFamily: "space",
+        fontWeight: 400,
+        fontSize: "12px",
+        lineHeight: 1.5,
+        letterSpacing: "0.04em",
+        textTransform: "uppercase",
+      },
+      xsmallSubhead: {
+        fontFamily: "space",
+        fontWeight: 400,
+        fontSize: "10px",
+        lineHeight: 1.5,
+        letterSpacing: "0.04em",
+        textTransform: "uppercase",
+      },
+      body: {
+        fontFamily: "lausanne",
+        fontWeight: 300,
+        fontSize: "18px",
+        lineHeight: 1.75,
+        letterSpacing: "2%",
+      },
+      bodyStrong: {
+        fontFamily: "lausanne",
+        fontWeight: 750,
+        fontSize: "20px",
+        lineHeight: 1.65,
+        letterSpacing: "1%",
+      },
+      small: {
+        fontFamily: "lausanne",
+        fontWeight: 300,
+        fontSize: "16px",
+        lineHeight: 1.75,
+      },
+      smallStrong: {
+        fontFamily: "lausanne",
+        fontWeight: 750,
+        fontSize: "16px",
+        lineHeight: 1.75,
+      },
+      buttonXlarge: {
+        fontFamily: "lausanne",
+        fontWeight: 300,
+        fontSize: "32px",
+        lineHeight: 1.65,
+        letterSpacing: "1%",
+      },
+      buttonLarge: {
+        fontFamily: "lausanne",
+        fontWeight: 750,
+        fontSize: "18px",
+        lineHeight: 1.65,
+        letterSpacing: "1%",
+      },
+      buttonSmall: {
+        fontFamily: "lausanne",
+        fontWeight: 750,
+        fontSize: "16px",
+        lineHeight: 1.65,
+        letterSpacing: "1%",
+      },
+    },
+  },
+});
+
+export const buttonRecipe = defineRecipe({
+  className: "button",
+  description: "Button styles in the design system",
+  base: {
+    _hover: {
+      opacity: 0.5,
+      cursor: "pointer",
+    },
+  },
+  variants: {
+    size: {
+      small: {
+        ...(textRecipe.variants?.variant.buttonSmall ?? {}),
+      },
+      large: {
+        ...(textRecipe.variants?.variant.buttonLarge ?? {}),
+      },
+      xlarge: {
+        ...(textRecipe.variants?.variant.buttonXlarge ?? {}),
+      },
+    },
+    underline: {
+      true: {
+        textDecoration: "underline",
+      },
+    },
+  },
+  defaultVariants: {
+    size: "small",
+  },
+});
 
 const globalFontface = defineGlobalFontface({
   "Space Mono": [
