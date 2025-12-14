2f96703c2878d05c96d82ece9532378278f35f98
Christopher N. Katoyi Kaba <me@christopher2k.dev>
2025-12-13 19:22:58 -0500

    WIP

--- apps/portfolio/src/routes/work-preview.$projectId.tsx
+++ apps/portfolio/src/routes/work-preview.$projectId.tsx
@@ -1,11 +1,48 @@
-import { createFileRoute } from "@tanstack/react-router";
+import {
+  RefreshRouteOnSave,
+  useLivePreview,
+} from "@payloadcms/live-preview-react";
+import {
+  createFileRoute,
+  useLoaderData,
+  useRouter,
+} from "@tanstack/react-router";
+import { env } from "@/env";
 import { ProjectView } from "@/features/projects/components/project-view";
+import { sdk } from "@/sdk";
 
 export const Route = createFileRoute("/work-preview/$projectId")({
   component: RouteComponent,
+  loader: async ({ params }) => {
+    const project = await sdk.findByID({
+      collection: "projects",
+      id: params.projectId,
+    });
+
+    return project;
+  },
   ssr: false,
 });
 
 function RouteComponent() {
-  return <ProjectView />;
+  const initialData = Route.useLoaderData();
+  const router = useRouter();
+  const { data } = useLivePreview({
+    initialData,
+    serverURL: env.VITE_PAYLOAD_URL,
+    depth: 2,
+  });
+
+  console.log("data", data);
+
+  const reload = () => {
+    router.invalidate({ forcePending: true, sync: true });
+  };
+
+  return (
+    <>
+      <RefreshRouteOnSave refresh={reload} serverURL={env.VITE_PAYLOAD_URL} />
+      <ProjectView />
+    </>
+  );
 }
